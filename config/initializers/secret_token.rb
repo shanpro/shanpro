13a23adfcbedd4fa1fd0d2b22d14578b6d2a777d
ubuntu <ubuntu@ubuntu.(none)>
2012-06-07 14:05:36 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shanpro::Application.config.secret_token = 'cd68f9d2996c03b0419b64fba5237a26ced31a3a0417dd04c4f440fdaad0463f86d875cfe2de33e0bc926f0a1b69251c185d2d40d4522f4f412b25c1084ca14b'
